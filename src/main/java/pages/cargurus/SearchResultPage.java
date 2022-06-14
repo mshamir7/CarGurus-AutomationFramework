e7bfea4e210cb53248f26330b19ed6718591d7ad
mshamir7 <shamirmashfik7@gmail.com>
2022-06-13 22:23:14 -0400

    Automated TC 23 through 26

--- src/main/java/pages/cargurus/SearchResultPage.java
+++ src/main/java/pages/cargurus/SearchResultPage.java
@@ -1,6 +1,7 @@
 package pages.cargurus;
 
 import base.CommonAPI;
+import org.omg.CORBA.PUBLIC_MEMBER;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -23,7 +24,7 @@ public class SearchResultPage extends CommonAPI {
     @FindBy(xpath = "//*[@id='cargurus-listing-search']/div/div/div[2]/div[2]/div[1]/div/div")
     private WebElement searchResultHeaderTextNewCarTitle;
 
-    @FindBy(css = "input[name='zip']")
+    @FindBy(xpath = "//input[@id='zip']")
     private WebElement zipCode;
 
     @FindBy(xpath = "//button[contains(text(),'Update Zip')]")
@@ -31,6 +32,45 @@ public class SearchResultPage extends CommonAPI {
 
     @FindBy(xpath = "//body/main[@id='main']/div[@id='cargurus-listing-search']/div[1]/div[1]/div[2]/div[2]/section[1]")
     private WebElement errorMessageBanner;
+
+    @FindBy(css = "#react-tabs-6")
+    private WebElement bodyStyleTab;
+
+    @FindBy(css = "#bodyTypeGroup")
+    private WebElement bodyStyleDropDownList;
+
+    @FindBy(xpath = "//li[@id='react-tabs-24']")
+    private WebElement byPriceTab;
+
+    @FindBy(css = "#minPrice")
+    private WebElement minPriceTextBox;
+
+    @FindBy(css = "#maxPrice")
+    private WebElement maxPriceTextBox;
+
+    @FindBy(xpath = "//body/main[@id='main']/div[@id='cargurus-listing-search']/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/form[1]/div[3]/button[1]")
+    private WebElement searchBtn;
+
+    @FindBy(css = "button[class='HaLmAx kMGxRh SUJbPV nM1LaL']")
+    private WebElement requestInfoBtn;
+
+    @FindBy(css = "input[name='firstName']")
+    private WebElement requestInfoFirstName;
+
+    @FindBy(css = "input[name='lastName']")
+    private WebElement requestInfoLastName;
+
+    @FindBy(css = "input[name='postalCode']")
+    private WebElement requestInfoPostalCode;
+
+    @FindBy(css = "input[name='emailAddress']")
+    private WebElement requestInfoEmailAddress;
+
+    @FindBy(css = "span[contains(text(),'Send')]")
+    private WebElement requestInfoSendBtn;
+
+
+
 
     public SearchResultPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
@@ -69,5 +109,65 @@ public class SearchResultPage extends CommonAPI {
 
     public boolean assertErrorMessageBanner(){
        return isPresent(errorMessageBanner);
+    }
+
+    public void clickBodyStyleTab(){
+        click(bodyStyleTab);
+    }
+
+    public void selectBodyStyleDropDownList(String option){
+        selectFromDropdown(bodyStyleDropDownList, option);
+    }
+
+    public void typeAndEnterZip(String text){
+        typeAndEnter(zipCode, text);
+    }
+
+    public void clickByPriceTab(){
+        click(byPriceTab);
+    }
+
+    public void clearMinPriceTextBox(){
+        clear(minPriceTextBox);
+    }
+
+    public void typeMinPrice(String text){
+        type(minPriceTextBox, text);
+    }
+
+    public void clearMaxPriceTextBox(){
+        clear(maxPriceTextBox);
+    }
+
+    public void typeMaxPrice(String text){
+        type(maxPriceTextBox, text);
+    }
+
+    public void typeZipCode(String text){
+        type(zipCode, text);
+    }
+
+    public void clickSearchBtn(){
+        click(searchBtn);
+    }
+
+    public void clickRequestInfoBtn(){
+        click(requestInfoBtn);
+    }
+
+    public  void typeRequestInfoFirstName(String text){
+        type(requestInfoFirstName, text);
+    }
+
+    public void typeRequestInfoLastName(String text){
+        type(requestInfoLastName, text);
+    }
+
+    public void typeRequestInfoPostalCode(String text){
+        type(requestInfoPostalCode, text);
+    }
+
+    public void typeRequestInfoEmailAddress(String text){
+        type(requestInfoEmailAddress, text);
     }
 }
--- src/test/java/apps/cargurus/FilterSearchResultsUsedCars.java
+++ src/test/java/apps/cargurus/FilterSearchResultsUsedCars.java
@@ -8,7 +8,7 @@ import pages.cargurus.HomePage;
 import pages.cargurus.SearchResultPage;
 import pages.cargurus.ShoppingForAUsedCarPage;
 
-public class SearchResults extends CommonAPI {
+public class FilterSearchResultsUsedCars extends CommonAPI {
 
     @Test
     public void searchUsedCarsFromShoppingForAUsedCarPage() {
@@ -25,11 +25,11 @@ public class SearchResults extends CommonAPI {
         waitFor(2);
         String actualSearchTitleText = getDriver().findElement(By.xpath("//body/main[@id='main']/div[@id='cargurus-listing-search']/div[1]/div[1]/div[1]/h1[1]")).getText();
         Assert.assertEquals("Used Acura ILX for Sale in Forest Hills, NY", actualSearchTitleText);
-
-
-        //Update Zipcode From SEARCH RESULTS PAGE TC022
-
         waitFor(2);
+
+
+    //Update Zipcode From SEARCH RESULTS PAGE TC022
+
         SearchResultPage searchResultPage = new SearchResultPage(getDriver());
         searchResultPage.clearZipCodeTextBox();
         searchResultPage.typeAndUpdateZipCode("11565");
@@ -38,12 +38,54 @@ public class SearchResults extends CommonAPI {
         String actualUpdateSearchTitleText = getDriver().findElement(By.xpath("//body/main[@id='main']/div[@id='cargurus-listing-search']/div[1]/div[1]/div[1]/h1[1]")).getText();
         Assert.assertEquals("Used Acura ILX for Sale in Malverne, NY", actualUpdateSearchTitleText);
 
-        //ERROR MESSAGE DISPLAYED USING INVALID ZIPCODE TC023
+
+    //ERROR MESSAGE DISPLAYED USING INVALID ZIPCODE TC023
+
 
         searchResultPage.clearZipCodeTextBox();
         searchResultPage.typeAndUpdateZipCode("00001");
         searchResultPage.clickUpdateZip();
         waitFor(2);
         Assert.assertTrue(searchResultPage.assertErrorMessageBanner());
+
+
+
+    //FILTER SEARCH BY BODY STYLE FROM SEARCH RESULTS PAGE TC024
+
+
+        searchResultPage.clickBodyStyleTab();
+        searchResultPage.selectBodyStyleDropDownList("Sedan");
+        searchResultPage.clearZipCodeTextBox();
+        searchResultPage.typeAndEnterZip("11565");
+        String actualText = getDriver().findElement(By.xpath("//em[contains(text(),'Sedans')]")).getText();
+        Assert.assertEquals("Sedans", actualText);
+        String actualText1 = getDriver().findElement(By.xpath("//em[contains(text(),'Malverne, NY')]")).getText();
+        Assert.assertEquals("Malverne, NY", actualText1);
+
+
+    //FILTER SEARCH BY PRICE FROM SEARCH RESULTS PAGE TC025
+
+
+        searchResultPage.clickByPriceTab();
+        searchResultPage.clearMinPriceTextBox();
+        waitFor(2);
+        searchResultPage.typeMinPrice("15000");
+        searchResultPage.clearMaxPriceTextBox();
+        waitFor(2);
+        searchResultPage.typeMaxPrice("25000");
+        searchResultPage.clearZipCodeTextBox();
+        searchResultPage.typeZipCode("11565");
+        searchResultPage.clickSearchBtn();
+        String acutal = getDriver().findElement(By.xpath("//div[contains(text(),'Price Search')]")).getText();
+        Assert.assertEquals("Price Search", acutal);
+
+    // NAVIGATE TO REQUEST INFO DIALOG BOX TC026
+
+        searchResultPage.clickRequestInfoBtn();
+        searchResultPage.typeRequestInfoFirstName("John");
+        searchResultPage.typeRequestInfoLastName("Smith");
+        searchResultPage.typeRequestInfoPostalCode("11565");
+        searchResultPage.typeRequestInfoEmailAddress("abcd1234@gmail.com");
+        Assert.assertTrue(getDriver().findElement(By.xpath("//span[contains(text(),'Send')]")).isDisplayed());
     }
 }
