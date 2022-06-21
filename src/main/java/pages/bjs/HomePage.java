4e026d862339871d2e534abcea65aed69bb82b09
kttheman1 <104694182+kttheman1@users.noreply.github.com>
2022-06-21 16:57:43 -0400

    new test

--- src/main/java/pages/bjs/HomePage.java
+++ src/main/java/pages/bjs/HomePage.java
@@ -8,13 +8,53 @@ import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends CommonAPI {
 
-  //ELEMENTS
+    @FindBy(css = ".login-state ml-1")
+    private WebElement signInBtn;
 
-public HomePage(WebDriver driver) {
+    @FindBy(css = "#searchTerm")
+    private WebElement searchBar;
+
+    @FindBy(css = ".fa fa-search")
+    private WebElement searchBtn;
+
+    @FindBy(css = ".cart-style")
+    private WebElement addToCartBtn;
+
+    @FindBy(css = ".club-name-b2c club-name-space")
+    private WebElement storeLocationBtn;
+
+//    @FindBy(css = "")
+//    private WebElement ;
+
+
+
+    public HomePage(WebDriver driver) {
     PageFactory.initElements(driver, this);
 }
 
-    //METHODS
+    public void clickSignInBtn() {
+        click(signInBtn);
+    }
+
+    public void typeSearchTerm(String text) {
+        type(searchBar, text);
+    }
+
+    public void clickSearchBtn() {
+        click(searchBtn);
+    }
+
+    public void clickAddToCartBtn() {
+        click(addToCartBtn);
+    }
+
+    public void clickStoreLocationBtn() {
+        click(storeLocationBtn);
+    }
+
+
+
+
 
 }
 
--- src/test/java/apps/bjs/LoginTest.java
+++ src/test/java/apps/bjs/LoginTest.java
@@ -1,15 +1,25 @@
 package apps.bjs;
 
 import base.CommonAPI;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.bjs.HomePage;
+import pages.bjs.LogInPage;
 
 public class LoginTest extends CommonAPI {
 
+        @Test
+        public void validLoginCred() {
+            pages.bjs.HomePage homePage = new HomePage(getDriver());
+            pages.bjs.LogInPage loginPage = new LogInPage(getDriver());
+            homePage.clickSignInBtn();
+            loginPage.typeEmailForLogin("kttheman1@aol.com");
+            loginPage.typePasswordForLogin("Tomsun@32!");
+            loginPage.clickLoginSignInBtn();
 
-}
+        }
+
+
+    }
 
 
